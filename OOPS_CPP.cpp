ced557fc20d734a6a2b23687841a9e39e77dbb2c
Souvik Mondal <souvikmondal1617@gmail.com>
2022-06-21 11:33:33 +0530

    creating hero class

---
+++ OOPS_CPP.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+using std::vector;
+using std::string;
+using std::max;
